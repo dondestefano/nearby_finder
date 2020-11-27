06f314c06ab792e7f1ceead277fc64da8653fcd6
Johan <johanlindstroom@icloud.com>
2020-11-27 15:32:04 +0100

    Safety push.
    NEW :bug:  Expected BEGIN_ARRAY but was BEGIN_OBJECT at line 1 column 2 path $

--- app/src/main/java/com/example/nearby_finder/PlacesService.kt
+++ app/src/main/java/com/example/nearby_finder/PlacesService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PlacesService {
-    @GET("nearbysearch/json?location=33.8670522, 151.1957362&radius=500&types=restaurant&key=AIzaSyBi9sglBeE2u7V-pUOWfm0su6a_H-poBqQ")
+    @GET("nearbysearch/json?location=33.8670522,133.1957362&radius=500&types=resturante&key=AIzaSyBi9sglBeE2u7V-pUOWfm0su6a_H-poBqQ")
     suspend fun getNearbyLocation(): Response<Places>
 }
