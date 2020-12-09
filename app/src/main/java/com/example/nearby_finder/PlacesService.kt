81249350b680ac3ecdd0c8e1d58ab69609d2c1e6
Johan <johanlindstroom@icloud.com>
2020-12-09 13:06:02 +0100

    Update PlacesService with working URL

--- app/src/main/java/com/example/nearby_finder/PlacesService.kt
+++ app/src/main/java/com/example/nearby_finder/PlacesService.kt
@@ -7,7 +7,8 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PlacesService {
-    @GET("nearbysearch/json?location=33.8670522,133.1957362&radius=500&types=restaurant&key=AIzaSyBi9sglBeE2u7V-pUOWfm0su6a_H-poBqQ")
+    // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=food&key=AIzaSyBi9sglBeE2u7V-pUOWfm0su6a_H-poBqQ"
+    @GET("nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=food&key=AIzaSyBi9sglBeE2u7V-pUOWfm0su6a_H-poBqQ")
     suspend fun getNearbyLocation(): Response<Places>
 
     //TODO Remove this after testing
