65489672dcaf9775824fbb38bcaea8d5ade4179a
dondestefano <dondestefano@users.noreply.github.com>
2020-12-14 19:22:09 +0100

    Fixes layout and image for RecylerView.

--- app/src/main/java/com/example/nearby_finder/PlacesViewModel.kt
+++ app/src/main/java/com/example/nearby_finder/PlacesViewModel.kt
@@ -10,9 +10,9 @@ class PlacesViewModel: ViewModel() {
 
     private fun createDummyData(): List<DummyData> {
 
-        val placeOne = DummyData("Pepe's Italian", "Pizzeria", 125, "http:image")
+        val placeOne = DummyData("Pepe's Italian", "Pizzeria", 125, "https://i.pinimg.com/originals/dc/ca/20/dcca201c915e6b8be4d5b9385c343662.jpg")
         val placeTwo = DummyData("Dirty Diego's Tacos", "Taqueria", 50, "http:image")
-        val placeThree = DummyData("Greasy Joe's Platter", "Grill", 500, "http:image")
+        val placeThree = DummyData("Greasy Joe's Platter", "Grill", 500, "https://media-cdn.tripadvisor.com/media/photo-s/10/59/74/be/greasy-spoon-hagagatan.jpg")
 
         return mutableListOf(placeOne, placeTwo, placeThree)
     }
