893e92f0594450dfd2613f6a0fa4b25a57425c4a
Johan <johanlindstroom@icloud.com>
2020-12-15 14:15:03 +0100

    replace fetchPlace with findCurrentPlace
    :alien: :alien: :alien: :alien: :alien:

--- app/src/main/java/com/example/nearby_finder/PlaceManager.kt
+++ app/src/main/java/com/example/nearby_finder/PlaceManager.kt
@@ -1,39 +1,36 @@
 package com.example.nearby_finder
 
 import android.Manifest
+import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.google.android.gms.common.api.ApiException
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.net.FetchPlaceRequest
 import com.google.android.libraries.places.api.net.FetchPlaceResponse
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
-import org.json.JSONException
-import org.json.JSONObject
-import java.net.URL
-import java.security.AccessController.getContext
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
 import java.util.*
 
 
 object PlaceManager {
+
     fun fetchPlace(context: Context) {
 
         // Create a new Places client instance.
         val placesClient = Places.createClient(context)
 
-        if (ActivityCompat.checkSelfPermission(MainActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+        if (ActivityCompat.checkSelfPermission((context as Activity?)!!, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((context as Activity?)!!, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ), 10)
         }
 
         // Define a Place ID.
@@ -42,7 +39,7 @@ object PlaceManager {
         // Specify the fields to return.
         val placeFields: List<Place.Field> = listOf(
                 Place.Field.ID,
-                Place.Field.NAME
+                Place.Field.NAME,
         )
 
         val currentPlace = FindCurrentPlaceRequest.newInstance(placeFields)
@@ -50,22 +47,22 @@ object PlaceManager {
         placesClient.findCurrentPlace(currentPlace)
 
         // Construct a request object, passing the place ID and fields array.
-        val request = FetchPlaceRequest.newInstance(placeId, placeFields)
+        //val request = FetchPlaceRequest.newInstance(placeId, placeFields)
 
-        placesClient.fetchPlace(request)
-                .addOnSuccessListener { response: FetchPlaceResponse ->
-                    val place = response.place
-                    Log.i("TAG", "Place found: ${place.name}")
+        placesClient.findCurrentPlace(currentPlace)
+                .addOnSuccessListener { response: FindCurrentPlaceResponse ->
+                    val place = response.placeLikelihoods
+                    Log.i("SUCCESS", "Place found: $place")
 
                 }.addOnFailureListener { exception: Exception ->
                     if (exception is ApiException) {
-                        Log.e(TAG, "Place not found: ${exception.message}")
+                        Log.e("ERR", "Place not found: ${exception.message}")
                         val statusCode = exception.statusCode
+                        Log.e("ERR", "Status code: $statusCode")
                         TODO("Handle error with given status code")
                     }
                 }
 
     }
-
 
 }
