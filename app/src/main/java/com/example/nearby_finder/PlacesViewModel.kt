fea2cafc31c8fdfcf83c7cd7efc22cec06b1861e
dondestefano <dondestefano@users.noreply.github.com>
2020-12-19 16:19:38 +0100

    Adds bubble sort to PlacesViewModel.

--- app/src/main/java/com/example/nearby_finder/PlacesViewModel.kt
+++ app/src/main/java/com/example/nearby_finder/PlacesViewModel.kt
@@ -20,27 +20,8 @@ class PlacesViewModel(private val repository: PlacesRepository): ViewModel() {
 
     private fun insertAll() = viewModelScope.launch {
         repository.deleteAll()
-        repository.insertAll(PlaceManager.getFetchedPlaces())
-    }
-
-    fun testNetwork() = viewModelScope.launch {
-        repository.deleteAll()
-        repository.insertAll(createDummyData())
-    }
-
-    // TODO: Remove this when Places call is in place.
-    private fun createDummyData(): List<PlaceItem> {
-
-        return if (NetworkManager.isNetworkConnected) {
-            val placeOne = PlaceItem("We got", "Pizzeria", "https://i.pinimg.com/originals/dc/ca/20/dcca201c915e6b8be4d5b9385c343662.jpg")
-            val placeTwo = PlaceItem("Internet", "Moj", "http:image")
-
-            mutableListOf(placeOne, placeTwo)
-        } else {
-            val placeThree = PlaceItem("No internet", "Grill",  "https://media-cdn.tripadvisor.com/media/photo-s/10/59/74/be/greasy-spoon-hagagatan.jpg")
-
-            mutableListOf(placeThree)
-        }
+        val list = bubbleSortList(PlaceManager.getFetchedPlaces())
+        repository.insertAll(list)
     }
 
     class PlacesViewModelFactory(private val repository: PlacesRepository) : ViewModelProvider.Factory {
@@ -69,5 +50,59 @@ class PlacesViewModel(private val repository: PlacesRepository): ViewModel() {
             }
 
         }
+    }
+
+    // TODO: Remove this when Places call is in place.
+    fun testNetwork() = viewModelScope.launch {
+        repository.deleteAll()
+        val list = bubbleSortList(createDummyData())
+        repository.insertAll(list)
+    }
+
+    private fun createDummyData(): MutableList<PlaceItem> {
+        return if (NetworkManager.isNetworkConnected) {
+            val placeTwo = PlaceItem("Internet", "Moj", "http:image")
+            val placeThree = PlaceItem("And this should be last", "Moj", "http:image")
+            val placeOne = PlaceItem("We got that", "Pizzeria", "https://i.pinimg.com/originals/dc/ca/20/dcca201c915e6b8be4d5b9385c343662.jpg")
+
+            mutableListOf(placeTwo, placeThree, placeOne)
+        } else {
+            val placeThree = PlaceItem("No internet", "Grill",  "https://media-cdn.tripadvisor.com/media/photo-s/10/59/74/be/greasy-spoon-hagagatan.jpg")
+
+            mutableListOf(placeThree)
+        }
+    }
+
+    private fun bubbleSortList(list: MutableList<PlaceItem>): MutableList<PlaceItem> {
+        var numSwaps = 0
+        var isSorted = false
+
+        var lastUnsorted = list.size - 1
+
+        if (list.size <= 1) {
+            return list
+        } else {
+            while (!isSorted) {
+                isSorted = true
+                for (i in 0 until lastUnsorted) {
+                    if (list[i].name.count() > list[i + 1].name.count()) {
+                        swapValues(list, i, i + 1)
+                        numSwaps++
+                        isSorted = false
+                    }
+                }
+                lastUnsorted--
+            }
+            for(i in list) {
+                println(i)
+            }
+            return list
+        }
+    }
+
+    private fun swapValues(list: MutableList<PlaceItem>, a: Int, b: Int) {
+        val temp = list[b]
+        list[b] = list[a]
+        list[a] = temp
     }
 }
